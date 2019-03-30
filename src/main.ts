f552ba246ddef169885b8c41f770d964fcc7020d
Peter Cai <peter@typeblog.net>
2019-03-30 15:19:43 +0800

    add Server and implemented database connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
-import express from "express";
+import Server from "./server";
 
-function main() {
-    let app = express();
-    app.listen(23456, () => {
-        console.log("Dat.ee backend is up and running at http://127.0.0.1:23456")
-    });
-}
-
-main()
+// Just load the server :)
+Server.setupRoutes();
+Server.listen(23456);
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import nano from "nano";
+
+interface ServerConfig {
+    couchdb: string
+}
+
+// Central manager of the entire backend server
+class DateeServer {
+    private app : express.Express;
+    private dbServer : nano.ServerScope;
+    constructor(config: ServerConfig) {
+        this.app = express();
+        this.dbServer = nano(config.couchdb);
+    }
+
+    public setupRoutes() {
+        this.app.get("/hello", (_, res) => {
+            res.send("hello!");
+        });
+    }
+
+    public listen(port: number) {
+        this.app.listen(port, () => {
+            console.log("Dat.ee backend server is up and running at " + port);
+        });
+    }
+
+    // Get a database object from CouchDB
+    // If it does not exist, this will create the corresponding db
+    public async getDatabase<T>(name: string): Promise<nano.DocumentScope<T>> {
+        let databases = await this.dbServer.db.list();
+        if (databases.indexOf(name) < 0) {
+            let resp = await this.dbServer.db.create(name);
+            if (!resp.ok) {
+                throw `Cannot create database ${name}`;
+            }
+        }
+        return this.dbServer.db.use(name);
+    }
+}
+
+// Export an singleton for use
+export default new DateeServer(require('../config.json'));
