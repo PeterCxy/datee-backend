01397b8977027659a5cf916e2e6a843d6f32a7c2
Peter Cai <peter@typeblog.net>
2019-04-20 16:24:40 +0800

    enable multi-process cluster for the client
    which means we have to pay attention to data consistency from now on

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+import cluster from "cluster";
+import os from "os";
 import Server from "./server";
 import "./controller/index";
 
-// Just load the server :)
-Server.setupRoutes().then(() => {
-    Server.listen(23456);
-});
+// Spawn several worker processes to distribute the load
+// This is needed because some of our computation might block
+// Note: Anything here inside our program SHOULD NOT
+//       DEPEND ON any internal memory state. Everything
+//       should be based on something in the database
+//       and should be written back to the database
+//       ASAP.
+if (cluster.isMaster) {
+    for (let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+} else {
+    // Just load the server :)
+    Server.setupRoutes().then(() => {
+        Server.listen(23456);
+    });
+}
