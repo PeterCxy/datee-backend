532df403728983fc7129dd3b2e64d6be769d1b8f
Thomas Cilloni <thomas.martin.cilloni@gmail.com>
2019-05-02 21:24:55 +0800

    Added time/location options to match model

--- src/model/match.ts
+++ src/model/match.ts
@@ -1,6 +1,21 @@
 export default interface Match {
     userID1: string,
     userID2: string,
+    // when the match was set
     date: number,
-    active: boolean,
+    // to reduce calulations with times. Flag is true if the match happened less than 24h ago
+    active: boolean,    
+    // list of potential dates proposed by the two
+    dates: Date[]
+}
+
+export interface Date {
+    // 1 or 2, the user who proposed it
+    madeBy: number,
+    // time of the meeting
+    date: number,
+    // location of the meeting
+    location: string,
+    // true if the other user agrees on it. Only 1 date can be agreed on
+    agreed: boolean
 }
